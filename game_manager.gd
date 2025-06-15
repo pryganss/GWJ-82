67198dd313c255b87f9966cd38d4ab6b468c072e
May May <thepullinger@gmail.com>
2025-06-15 12:33:37 +0100

    add level completion logic

--- game_manager.gd
+++ game_manager.gd
@@ -3,8 +3,16 @@ extends Node
 var player: CharacterBody2D
 
 var goal_collected: bool = false
+
+func load_level():
+	goal_collected = false
+
 func player_spotted(spotter: Node) -> void:
 	print("Player spotted by ", spotter)
 
 func exit_attempted() -> void:
-	print("Player attempted to exit")
+	match goal_collected:
+		true:
+			print("player can complete level")
+		false:
+			print("Player attempted to exit")
