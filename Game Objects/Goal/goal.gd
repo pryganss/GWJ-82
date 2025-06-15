d8a05adb4c9b1d38e451d70e8c32074dbc8cc9b3
May May <thepullinger@gmail.com>
2025-06-15 12:32:12 +0100

    change goal collected function to lambda

--- Game Objects/Goal/goal.gd
+++ Game Objects/Goal/goal.gd
@@ -4,7 +4,7 @@ signal collect_goal
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	collect_goal.connect(GameManager.goal_collected)
+	collect_goal.connect(func(): GameManager.goal_collected = true)
 
 func _on_body_entered(body:Node2D) -> void:
 	if body.is_in_group("player"):
--- game_manager.gd
+++ game_manager.gd
@@ -2,11 +2,9 @@ extends Node
 
 var player: CharacterBody2D
 
+var goal_collected: bool = false
 func player_spotted(spotter: Node) -> void:
 	print("Player spotted by ", spotter)
-
-func goal_collected() -> void:
-	print("Player collected the goal")
 
 func exit_attempted() -> void:
 	print("Player attempted to exit")
